10a402d1bbd5c1e2ab99081af614e6b25341e557
lingeaniket <linge.aniket.10@gmail.com>
2023-01-04 09:24:36 +0530

    new Theatres

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,62 @@
 $(document).ready(function () {
         $("#header").load('HeaderFooter/header.html');
         $("#header1").load('/HeaderFooter/header.html');
-        //Avatar2
+});
+
+//An Action Hero
+$(document).ready(function () {
+        //3Jan23
+        $("#cityPrideSataraRoadAnActionHero-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceAnActionHero');
+        $("#cinePolisSeasonsAnActionHero-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceAnActionHero');
+
+        //4Jan23
+        $("#cityPrideSataraRoadAnActionHero-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceAnActionHero');
+        $("#cinePolisSeasonsAnActionHero-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceAnActionHero');
+});
+
+$(document).ready(function loadMovieInfoAnActionHero() {
+        $('#openAnActionHeroInfo').load('/MovieInfo/movie.html #anActionHero');
+});
+
+//Avatar2:The Way Of Water
+$(document).ready(function () {
+        //3Jan23
         $("#cityPrideSataraRoadAvatar2-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceAvatar2');
         $("#cinePolisSeasonsAvatar2-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceAvatar2');
 
+        //4Jan23
         $("#cityPrideSataraRoadAvatar2-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceAvatar2');
         $("#cinePolisSeasonsAvatar2-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceAvatar2');
-
-        //Drishyam2
-        $("#cityPrideSataraRoadDrishyam2-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceDrishyam2');
-        $("#cinePolisSeasonsDrishyam2-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceDrishyam2');
-
-        $("#cityPrideSataraRoadDrishyam2-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceDrishyam2');
-        $("#cinePolisSeasonsDrishyam2-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceDrishyam2');
 });
-var movieName = "";
+
 $(document).ready(function loadMovieInfoAvatar2() {
         $('#openAvatar2Info').load('/MovieInfo/movie.html #avatar2');
 });
 
+//Connect
+$(document).ready(function () {
+        //3Jan23
+        $("#cityPrideSataraRoadConnect-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceConnect');
+        $("#cinePolisSeasonsConnect-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceConnect');
 
-$(document).ready(function loadMovieInfoVed() {
-        $('#openVedInfo').load('/MovieInfo/movie.html #ved');
+        //4Jan23
+        $("#cityPrideSataraRoadConnect-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceConnect');
+        $("#cinePolisSeasonsConnect-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceConnect');
+});
+
+$(document).ready(function loadMovieInfoConnect() {
+        $('#openConnectInfo').load('/MovieInfo/movie.html #connect');
+});
+
+//Drishyam2
+$(document).ready(function () {
+        //3Jan23
+        $("#cityPrideSataraRoadDrishyam2-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceDrishyam2');
+        $("#cinePolisSeasonsDrishyam2-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceDrishyam2');
+
+        //4Jan23
+        $("#cityPrideSataraRoadDrishyam2-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceDrishyam2');
+        $("#cinePolisSeasonsDrishyam2-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceDrishyam2');
 });
 
 $(document).ready(function loadMovieInfoDrishyam2() {
@@ -30,22 +64,37 @@ $(document).ready(function loadMovieInfoDrishyam2() {
         $('#openDrishyam2Info').load('/MovieInfo/movie.html #drishyam2');
 });
 
-$(document).ready(function loadMovieInfoConnect() {
-        $('#openConnectInfo').load('/MovieInfo/movie.html #connect');
+//Hit2
+$(document).ready(function () {
+        //3Jan23
+        $("#cityPrideSataraRoadHit2-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceHit2');
+        $("#cinePolisSeasonsHit2-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceHit2');
+
+        //4Jan23
+        $("#cityPrideSataraRoadHit2-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceHit2');
+        $("#cinePolisSeasonsHit2-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceHit2');
 });
 
 $(document).ready(function loadMovieInfoHit2() {
         $('#openHit2Info').load('/MovieInfo/movie.html #hit2');
 });
 
-$(document).ready(function loadMovieInfoAnActionHero() {
-        $('#openAnActionHeroInfo').load('/MovieInfo/movie.html #anActionHero');
+//Ved
+$(document).ready(function () {
+        //3Jan23
+        $("#cityPrideSataraRoadVed-3Jan23").load('/Theaters/3Jan23/cityPrideSataraRoad.html #showSpaceVed');
+        $("#cinePolisSeasonsVed-3Jan23").load('/Theaters/3Jan23/cinePolisSeasons.html #showSpaceVed');
+
+        //4Jan23
+        $("#cityPrideSataraRoadVed-4Jan23").load('/Theaters/4Jan23/cityPrideSataraRoad.html #showSpaceVed');
+        $("#cinePolisSeasonsVed-4Jan23").load('/Theaters/4Jan23/cinePolisSeasons.html #showSpaceVed');
 });
+
+$(document).ready(function loadMovieInfoVed() {
+        $('#openVedInfo').load('/MovieInfo/movie.html #ved');
+});
+
 
 $(document).ready(function loadMovieInfoProjapati() {
         $('#openProjapatiInfo').load('/MovieInfo/movie.html #projapati');
 });
-
-
-
-
